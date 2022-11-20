2bbfa035b66df2890ce170bc58a470af35139263
Nicolas <nicolas.arvani@gmail.com>
2022-11-20 20:02:57 -0300

    Mudança de cor de fundo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -330,6 +330,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(1018, 629);
             this.Controls.Add(this.picChave);
             this.Controls.Add(this.chkGerarChaveAleatoria);
